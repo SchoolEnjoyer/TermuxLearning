ae21f969a6f9cac8b07937c597d1c48c5302767a
SchoolEnjoyer <102326017+SchoolEnjoyer@users.noreply.github.com>
2022-03-24 21:17:23 +0100

    Update german2.py

--- german2.py
+++ german2.py
@@ -1,90 +1,95 @@
+from colorama import Fore
 import random
 import os
 from time import sleep
+
+colorama.init()
+
+os.system("clear")   
 
 def start():
     var = random.choice("1234567890")
 
     if var == "1":
         print(var + " Przykład")
-        print("Iść")
+        print(Fore.YELLOW + "Iść" + Fore.WHITE)
         input("1 forma: ")
         input("3 forma: ")
         print("gehen / gegangen")
         input()
-        os.system("cls")
+        os.system("clear")   
     elif var == "2":
         print(var + " Przykład")
-        print("Wygrywać")
+        print(Fore.YELLOW + "Wygrywać" + Fore.WHITE)
         input("1 forma: ")
         input("3 forma: ")     
         print("gewinnen / gewonnen")
         input()
-        os.system("cls")     
+        os.system("clear")      
     elif var == "3":
         print(var + " Przykład")
-        print("Podlewać")
+        print(Fore.YELLOW + "Podlewać" + Fore.WHITE)
         input("1 forma: ")
         input("3 forma: ")
         print("gießen / gegossen")
         input()
-        os.system("cls")          
+        os.system("clear")           
     elif var == "4":
         print(var + " Przykład")
-        print("Mieć")
+        print(Fore.YELLOW + "Mieć" + Fore.WHITE)
         input("1 forma: ")
         input("3 forma: ")
         print("haben / gehabt")
         input()
-        os.system("cls")          
+        os.system("clear")        
     elif var == "5":
         print(var + " Przykład")
-        print("Pomagać")
+        print(Fore.YELLOW + "Pomagać" + Fore.WHITE)
         input("1 forma: ")
         input("3 forma: ")
         print("helfen / geholfen")
         input()
-        os.system("cls")          
+        os.system("clear")           
     elif var == "6":
         print(var + " Przykład")
-        print("Przychodzić")
+        print(Fore.YELLOW + "Przychodzić" + Fore.WHITE)
         input("1 forma: ")
         input("3 forma: ")
         print("kommen / gekommen")
         input()
-        os.system("cls")          
+        os.system("clear")           
     elif var == "7":
         print(var + " Przykład")
-        print("Biegać")
+        print(Fore.YELLOW + "Biegać" + Fore.WHITE)
         input("1 forma: ")
         input("3 forma: ")
         print("laufen / gelaufen")
         input()
-        os.system("cls")          
+        os.system("clear")           
     elif var == "8":
         print(var + " Przykład") 
-        print("Czytać")
+        print(Fore.YELLOW + "Czytać" + Fore.WHITE)
         input("1 forma: ")
         input("3 forma: ")
         print("lesen / gelesen")
         input()
-        os.system("cls")                 
+        os.system("clear")                 
     elif var == "9":
         print(var + " Przykład") 
-        print("Leżeć")
+        print(Fore.YELLOW + "Leżeć" + Fore.WHITE)
         input("1 forma: ")
         input("3 forma: ")
         print("liegen / gelegen")
         input()
-        os.system("cls")                 
+        os.system("clear")                  
     elif var == "0":
         print(var + " Przykład") 
-        print("Brać / Wziąść")
+        print(Fore.YELLOW + "Brać / Wziąść" + Fore.WHITE)
         input("1 forma: ")
         input("3 forma: ")
         print("nehmen / genommen")
         input()
-        os.system("cls")          
+        os.system("clear")          
 
 
 while True:
